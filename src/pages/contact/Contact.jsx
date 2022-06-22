d06ae5463b0066b9986be8aa9015b877be3e6ce0
antomag <antonellamagnano@gmail.com>
2022-06-22 17:32:45 -0300

    formulario para contacto

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,9 +1,26 @@
 import "./Contact.css";
 
 import React from "react";
+import imgFondoTitulo from "../../img/fondo-titulo.png";
+import Form from "../../form/Form";
 
 const Contact = () => {
-  return <div></div>;
+  return <>
+    <div className="containerTitulo">
+      <img src={imgFondoTitulo} alt="fondo" className="imgFondoTitulo"/>
+      <h1 className="tituloSeccion">CONTACTO</h1>
+    </div>
+    <Form/>
+    <div className="containerEmail">
+      <h2>E-mail</h2>
+      <p>info@cgiconsulting.co</p>
+      <p>ingenieria@cgiconsulting.co</p>
+      <p>seguros@cgiconsulting.co</p>
+      <p>energia@cgiconsulting.co</p>
+      <p>inmuebles@cgiconsulting.co</p>
+    </div>
+    {/* footer */}
+  </>;
 };
 
 export default Contact;
