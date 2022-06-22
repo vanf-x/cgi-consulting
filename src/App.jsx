dbfd5e77439716f6d9c20d45dc18dde27cfd272e
Clonazepunk <facundoaguirre1989@gmail.com>
2022-06-22 11:43:34 -0300

    creando navegación

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import './App.css'
-import Home from './components/home/Home'
-import Nav from './components/nav/Nav'
-function App() {
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/home/Home";
+import AboutUs from "./components/aboutUs/AboutUs";
+import Solutions from "./components/solutions/Solutions";
+import Services from "./components/services/Services";
+import Clients from "./components/clients/Clients";
+import Contact from "./components/contact/Contact";
+import Nav from "./components/nav/Nav";
 
+function App() {
+  // INICIO NOSOTROS SOLUCIONES NAV
   return (
     <div>
-      <Home/>
-      <Nav/>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/inicio" element={<Home />} />
+          <Route path="/nosotros" element={<AboutUs />} />
+          <Route path="/soluciones" element={<Solutions />} />
+          <Route path="/servicios" element={<Services />} />
+          <Route path="/clientes" element={<Clients />} />
+          <Route path="/contacto" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/solutions/Solutions.jsx
+++ src/components/solutions/Solutions.jsx
@@ -2,7 +2,7 @@ import "./Solutions.css"
 const Solutions = () => {
   return (
     <div>
-      
+      <h1>SOLUCIONES</h1>
     </div>
   )
 }
