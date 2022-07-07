f33d47e6ef457cc91d0c748630fcf8582b533a5a
antomag <antonellamagnano@gmail.com>
2022-07-07 18:05:34 -0300

    required

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -7,11 +7,11 @@ const Form = () => {
     <Container>
       <h1 className='titleForm'>Comunícate con nosotros</h1>
       <form action="https://formsubmit.co/info@cgiconsulting.co" method="POST" className='containerInputs'>
-        <TextField className="outlined-basic-nombre" label="Nombre" name="Nombre" variant="outlined" type="text"/>
-        <TextField className="outlined-basic-correo" label="Correo Electronico" name="email" variant="outlined" type="email"/>
-        <TextField className="outlined-basic-tel" label="Telefono" name="Telefono" variant="outlined" type="tel"/>
-        <TextField className="outlined-basic-org" label="Organizacion" name="Organizacion" variant="outlined" type="text"/>
-        <TextField className="outlined-basic-mensaje" label="Mensaje" name="Mensaje" variant="outlined" type="text"/>
+        <TextField className="outlined-basic-nombre" label="Nombre" name="Nombre" variant="outlined" type="text" required/>
+        <TextField className="outlined-basic-correo" label="Correo Electronico" name="email" variant="outlined" type="email" required/>
+        <TextField className="outlined-basic-tel" label="Telefono" name="Telefono" variant="outlined" type="tel" required/>
+        <TextField className="outlined-basic-org" label="Organizacion" name="Organizacion" variant="outlined" type="text" required/>
+        <TextField className="outlined-basic-mensaje" label="Mensaje" name="Mensaje" variant="outlined" type="text" required/>
         <Button className="send" variant="contained" type='submit'>ENVIAR</Button> 
         <input type="hidden" name="_next" value="http://www.cgiconsulting.co/"></input>
         <input type="hidden" name="_captcha" value="false"></input>
